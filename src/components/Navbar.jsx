94484aa51761dc513f306ea5c83637a02de9343d
Lokesh Vasnik <91425128+lokeshvasnik@users.noreply.github.com>
2025-07-08 23:09:45 +0530

    add: navbar login for is auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,40 @@
+import { useState } from "react";
 import { Link } from "react-router-dom"
 import Button from "./Button"
+import avatar from '../assets/profile.jpg'
 
 const Navbar = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
   return (
     <nav className="flex justify-between p-5 border-b-2 border-gray-200">
-        <div>
-            <h1 className="font-bold"><Link to="/">CodeUp</Link></h1>
-        </div>
-        <div>
-            <ul className="inline-flex space-x-5 mx-4">
-                <li>Features</li>
-                <li>Pricing</li>
-                <li>Support</li>
-            </ul>
-            <Button className="bg-blue-400 mx-2 rounded-md text-white cursor-pointer">
-              <Link to="/login">Log in</Link>
-            </Button>
-            <Button className="bg-gray-300 rounded-md text-black cursor-pointer">
-              <Link to="/register">Sign Up</Link>
-            </Button>
-        </div>
+      <div>
+        <h1 className="font-bold"><Link to="/">CodeUp</Link></h1>
+      </div>
+      <div>
+        {isAuthenticated ? <ul className="inline-flex space-x-5 mx-4">
+          <li>Dashboard</li>
+          <li>Roadmaps</li>
+          <li>Challenges</li>
+          <li>Community</li>
+          <li>Resources</li>
+          <Button className="bg-blue-400 py-2 mx-2 rounded-md text-white cursor-pointer">
+          </Button>
+          <Button className="bg-gray-300 py-2 rounded-md text-black cursor-pointer w-10">
+          </Button>
+        </ul> : <ul className="inline-flex space-x-5 mx-4">
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Support</li>
+          <Button className="bg-blue-400 mx-2 rounded-md text-white cursor-pointer">
+            <Link to="/login">Log in</Link>
+          </Button>
+          <Button className="bg-gray-300 rounded-md text-black cursor-pointer">
+            <Link to="/register">Sign Up</Link>
+          </Button>
+        </ul>}
+
+      </div>
     </nav>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,9 @@
+
+
 const Login = () => {
+
+
+
   return (
     <div>Login</div>
   )
